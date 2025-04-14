3687ca0473147ab2d058d45cbf9702818c52209b
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-14 13:32:41 +0300

    Created function for reg and auth

--- server.py
+++ server.py
@@ -11,3 +11,14 @@ class User:
         self.connection_string = ('DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=StatusTester;Trusted_Connection=yes')
         self.conn = pyodbc.connect(self.connection_string)
         self.cursor = self.conn.cursor()
+
+    def register_user(self):
+        self.cursor.execute('INSERT INTO Users (Username, Email, Password) VALUES (?, ?, ?)',(self.username, self.email, self.password))
+        self.conn.commit()
+
+    def check_login(self, input_password):
+        self.cursor.execute('SELECT Password FROM Users WHERE Username = ?', (self.username,))
+        result = self.cursor.fetchone()
+        if result:
+            return result[0] == input_password
+        return False
