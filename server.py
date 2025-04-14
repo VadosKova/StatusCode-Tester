f9a721b0727599d3aea2189c43ce1c7850ab1d15
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-14 13:30:04 +0300

    Created file for server and class for User

---
+++ server.py
@@ -0,0 +1,13 @@
+import socket
+import jsonpickle
+import pyodbc
+
+class User:
+    def __init__(self, username=None, email=None, password=None):
+        self.username = username
+        self.email = email
+        self.password = password
+
+        self.connection_string = ('DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=StatusTester;Trusted_Connection=yes')
+        self.conn = pyodbc.connect(self.connection_string)
+        self.cursor = self.conn.cursor()
