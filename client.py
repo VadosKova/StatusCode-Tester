c8f31b78ec549cb551027910c6bd1b5df63484bb
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-14 14:36:33 +0300

    Created class for gui

--- client.py
+++ client.py
@@ -24,3 +24,21 @@ EXAMPLE_QUESTIONS = {
         }
     ]
 }
+
+class StatusCodeTester:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("StatusCode Tester")
+        self.root.geometry("600x400")
+        self.root.configure(bg='#f0f0f0')
+
+        self.button_font = ('Arial', 10)
+        self.label_font = ('Arial', 10)
+        self.header_font = ('Arial', 14, 'bold')
+
+        self.current_user = None
+        self.current_test = None
+        self.current_question_index = 0
+        self.correct_answers = 0
+
+        self.start_screen()
